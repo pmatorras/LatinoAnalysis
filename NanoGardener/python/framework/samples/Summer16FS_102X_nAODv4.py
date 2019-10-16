9859902983f9d2ce969386cfb8b108e6f2ab00c1
scodella <luca.scodellaro@cern.ch>
2019-10-16 12:07:29 +0200

    update signal samples

--- NanoGardener/python/framework/samples/Summer16FS_102X_nAODv4.py
+++ NanoGardener/python/framework/samples/Summer16FS_102X_nAODv4.py
@@ -4,9 +4,20 @@ Samples = {}
 
 #### T2tt
 
-Samples['T2tt__mStop-400to1200']           = {'nanoAOD' : '/SMS-T2tt_mStop-400to1200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16NanoAODv4-PUSummer16v3Fast_Nano14Dec2018_102X_mcRun2_asymptotic_v6-v1/NANOAODSIM'}
+#Samples['T2tt__mStop-150to250']       = {'nanoAOD' : ''}
+#Samples['T2tt__mStop-250to350']       = {'nanoAOD' : ''}
+#Samples['T2tt__mStop-350to400']       = {'nanoAOD' : ''}
+Samples['T2tt__mStop-400to1200']       = {'nanoAOD' : '/SMS-T2tt_mStop-400to1200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16NanoAODv4-PUSummer16v3Fast_Nano14Dec2018_102X_mcRun2_asymptotic_v6-v1/NANOAODSIM'}
+Samples['T2tt__mStop-1200to2000']      = {'nanoAOD' : '/SMS-T2tt_mStop-1200to2000_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16NanoAODv4-PUSummer16v3Fast_Nano14Dec2018_102X_mcRun2_asymptotic_v6-v1/NANOAODSIM'}
 
-#### pm
+Samples['T2bW']                       = {'nanoAOD' : '/SMS-T2bW_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16NanoAODv4-PUSummer16v3Fast_Nano14Dec2018_102X_mcRun2_asymptotic_v6-v1/NANOAODSIM'}
+
+#### Chipm
+
+#Samples['TChipmSlepSnu']              = {'nanoAOD' : ''}
+#Samples['TChipmSlepSnu_mC1_825_1500'] = {'nanoAOD' : ''}
+#Samples['TChipmWW']                   = {'nanoAOD' : ''}
+
 
 
 
