78e4d1f98219bbe24213cb2891ec7656f3582af5
Luca <luca.scodellaro@cern.ch>
2019-12-11 13:02:52 +0100

    update susy nanoAOD datasets

--- NanoGardener/python/framework/samples/Autumn18FS_102X_nAODv4_v16.py
+++ NanoGardener/python/framework/samples/Autumn18FS_102X_nAODv4_v16.py
@@ -8,14 +8,14 @@ Samples['T2tt__mStop-150to250']       = {'nanoAOD' : '/SMS-T2tt_mStop-150to250_T
 Samples['T2tt__mStop-250to350']       = {'nanoAOD' : '/SMS-T2tt_mStop-250to350_TuneCP2_13TeV-madgraphMLM-pythia8/RunIIAutumn18NanoAODv4-PUFall18Fast_Nano14Dec2018_102X_upgrade2018_realistic_v16-v3/NANOAODSIM'}
 Samples['T2tt__mStop-350to400']       = {'nanoAOD' : '/SMS-T2tt_mStop-350to400_TuneCP2_13TeV-madgraphMLM-pythia8/RunIIAutumn18NanoAODv4-PUFall18Fast_Nano14Dec2018_102X_upgrade2018_realistic_v16-v3/NANOAODSIM'}
 Samples['T2tt__mStop-400to1200']       = {'nanoAOD' : '/SMS-T2tt_mStop-400to1200_TuneCP2_13TeV-madgraphMLM-pythia8/RunIIAutumn18NanoAODv4-PUFall18Fast_Nano14Dec2018_102X_upgrade2018_realistic_v16-v2/NANOAODSIM'}
-Samples['T2tt__mStop-1200to2000']      = {'nanoAOD' : '/SMS-T2tt_mStop-1200to2000_TuneCP2_13TeV-madgraphMLM-pythia8/RunIIAutumn18NanoAODv4-PUFall18Fast_Nano14Dec2018_102X_upgrade2018_realistic_v16-v2/NANOAODSIM'}
+#Samples['T2tt__mStop-1200to2000']      = {'nanoAOD' : '/SMS-T2tt_mStop-1200to2000_TuneCP2_13TeV-madgraphMLM-pythia8/RunIIAutumn18NanoAODv4-PUFall18Fast_Nano14Dec2018_102X_upgrade2018_realistic_v16-v2/NANOAODSIM'}
 
-Samples['T2bW']                       = {'nanoAOD' : '/SMS-T2bW_TuneCP2_13TeV-madgraphMLM-pythia8/RunIIAutumn18NanoAODv4-PUFall18Fast_Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/NANOAODSIM'}
+#Samples['T2bW']                       = {'nanoAOD' : '/SMS-T2bW_TuneCP2_13TeV-madgraphMLM-pythia8/RunIIAutumn18NanoAODv4-PUFall18Fast_Nano14Dec2018_102X_upgrade2018_realistic_v16-v1/NANOAODSIM'}
 
 #### Chipm
 
-#Samples['TChipmSlepSnu']              = {'nanoAOD' : ''}
-#Samples['TChipmSlepSnu_mC1_825_1500'] = {'nanoAOD' : ''}
+Samples['TChipmSlepSnu']              = {'nanoAOD' : ''}
+Samples['TChipmSlepSnu_mC1_825_1500'] = {'nanoAOD' : ''}
 #Samples['TChipmWW']                   = {'nanoAOD' : ''}
 
 
