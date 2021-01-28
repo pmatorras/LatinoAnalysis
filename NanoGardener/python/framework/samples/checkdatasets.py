29c6bf52a420c91cd7b02d5e3f2dc3901d56a7b5
scodella <luca.scodellaro@cern.ch>
2021-01-28 17:24:43 +0100

    add McM check

--- NanoGardener/python/framework/samples/checkdatasets.py
+++ NanoGardener/python/framework/samples/checkdatasets.py
@@ -4,22 +4,29 @@ import subprocess
 import math
 from optparse import OptionParser
 import optparse
+import sys
+sys.path.append('/afs/cern.ch/cms/PPD/PdmV/tools/McM/')
+from rest import McM
+
+mcm = McM(dev=True)
+
 if 'pmatorra' in os.environ.get('USER'):
     cmssw_directory= '/afs/cern.ch/work/p/pmatorra/private/CMSSW_10_6_19/'
 else:
-    cmssw_directory = '/afs/cern.ch/work/s/scodella/SUSY/CMSSW_10_2_14/'
+    cmssw_directory = '/afs/cern.ch/work/s/scodella/SUSY/CMSSW_10_6_19_patch2/'
+
 gardening_directory = 'src/LatinoAnalysis/NanoGardener/python/framework/samples/'
 production_directory = 'src/LatinoAnalysis/NanoProducer/python/samples/'
 
-campaigns = { 'UL16'  : { 'Data' : { 'AOD'    : '21Feb2020_UL2016-',     'MINIAOD'    : '21Feb2020_UL2016-',        'NANOAOD'    : 'UL2016_MiniAODv1_NanoAODv2' },
-                          'MC'   : { 'AODSIM' : 'RunIISummer20UL16RECO', 'MINIAODSIM' : 'RunIISummer20UL16MiniAOD', 'NANOAODSIM' : 'RunIISummer20UL16NanoAODv2' },    
-                          'FS'   : { 'AODSIM' : '',                      'MINIAODSIM' : '',                         'NANOAODSIM' : ''                           }, },
-              'UL17'  : { 'Data' : { 'AOD'    : '09Aug2019_UL2017-',     'MINIAOD'    : '09Aug2019_UL2017-',        'NANOAOD'    : 'UL2017_MiniAODv1_NanoAODv2' },             
-                          'MC'   : { 'AODSIM' : 'RunIISummer20UL17RECO', 'MINIAODSIM' : 'RunIISummer20UL17MiniAOD', 'NANOAODSIM' : 'RunIISummer20UL17NanoAODv2' },
-                          'FS'   : { 'AODSIM' : '',                      'MINIAODSIM' : '',                         'NANOAODSIM' : ''                           }, }, 
-              'UL18'  : { 'Data' : { 'AOD'    : '12Nov2019_UL2018-',     'MINIAOD'    : '12Nov2019_UL2018-',        'NANOAOD'    : 'UL2018_MiniAODv1_NanoAODv2' },             
-                          'MC'   : { 'AODSIM' : 'RunIISummer20UL18RECO', 'MINIAODSIM' : 'RunIISummer20UL18MiniAOD', 'NANOAODSIM' : 'RunIISummer20UL18NanoAODv2' },
-                          'FS'   : { 'AODSIM' : '',                      'MINIAODSIM' : '',                         'NANOAODSIM' : ''                           }, }, 
+campaigns = { 'UL16'  : { 'Data' : { 'AOD'    : '21Feb2020_UL2016-',     'MINIAOD'    : '21Feb2020_UL2016-',        'NANOAOD'    : 'UL2016_MiniAODv1_NanoAODv2'                                     },
+                          'MC'   : { 'AODSIM' : 'RunIISummer20UL16RECO', 'MINIAODSIM' : 'RunIISummer20UL16MiniAOD', 'NANOAODSIM' : 'RunIISummer20UL16NanoAODv2', 'GEN' : 'RunIISummer20UL16*GEN' },    
+                          'FS'   : { 'AODSIM' : '',                      'MINIAODSIM' : '',                         'NANOAODSIM' : ''                          , 'GEN' : ''                         }, },
+              'UL17'  : { 'Data' : { 'AOD'    : '09Aug2019_UL2017-',     'MINIAOD'    : '09Aug2019_UL2017-',        'NANOAOD'    : 'UL2017_MiniAODv1_NanoAODv2'                                     },             
+                          'MC'   : { 'AODSIM' : 'RunIISummer20UL17RECO', 'MINIAODSIM' : 'RunIISummer20UL17MiniAOD', 'NANOAODSIM' : 'RunIISummer20UL17NanoAODv2', 'GEN' : 'RunIISummer20UL17*GEN' },
+                          'FS'   : { 'AODSIM' : '',                      'MINIAODSIM' : '',                         'NANOAODSIM' : ''                          , 'GEN' : ''                         }, }, 
+              'UL18'  : { 'Data' : { 'AOD'    : '12Nov2019_UL2018-',     'MINIAOD'    : '12Nov2019_UL2018-',        'NANOAOD'    : 'UL2018_MiniAODv1_NanoAODv2'                                     },             
+                          'MC'   : { 'AODSIM' : 'RunIISummer20UL18RECO', 'MINIAODSIM' : 'RunIISummer20UL18MiniAOD', 'NANOAODSIM' : 'RunIISummer20UL18NanoAODv2', 'GEN' : 'RunIISummer20UL18*GEN' },
+                          'FS'   : { 'AODSIM' : '',                      'MINIAODSIM' : '',                         'NANOAODSIM' : ''                          , 'GEN' : ''                         }, }, 
             }
 
 # Main
@@ -82,8 +89,9 @@ if __name__ == '__main__':
         
         tiers = [ ]
         for tier in [ 'NANOAOD', 'MINIAOD', 'AOD' ]:
-            if opt.tier!='miniAOD' or tier!='NANOAODSIM': 
+            if opt.tier!='miniAOD' or tier!='NANOAOD': 
                 tiers.append(tier+Sim)
+
         sample_directory = production_directory if opt.tier=='miniAOD' else gardening_directory 
 
         exec(open(opt.directory+sample_directory+opt.samplefile.replace('.py', '')+'.py').read())
@@ -119,11 +127,12 @@ if __name__ == '__main__':
             datasetFound = ''
             datasetFlag = '_'+Samples[sample][opt.tier].split('/')[2]
 
+            if period!='': period = '_'+period
+            
+
             if len(datasetsFound)==1:
                 datasetFound = datasetsFound[0]
-                #print datasetFound
-                if 1==1:#opt.verbose:
-                    1<2
+                if opt.verbose:
                     print 'Dataset found for sample', process+period, 'in campaign', campaign[thistier], '-->', datasetFound
 
             elif len(datasetsFound)>1:
@@ -146,12 +155,35 @@ if __name__ == '__main__':
                 #    query_output = subprocess.check_output('dasgoclient -query='+query, shell=True)
                 #exit()
             else:   
-                print 'Warning: no dataset found for sample', process+'_'+period, 'in campaign', campaign[thistier]
+                print 'Warning: no dataset found for sample', process+period, 'in campaign', campaign[thistier]
                 if len(parentsFound)>0:
                     print '         available parents are', parentsFound
 
+                else:
+ 
+                    mcm_status = 0
+
+                    mcm_query = 'dataset_name='+process+'&prepid=*'+campaign['GEN']+'*'
+                    mcm_query = mcm_query.replace('_PSweights', '')
+                    if 'Tune' not in mcm_query: mcm_query = mcm_query.replace('13TeV', 'TuneCP5_13TeV')
+                    requests = mcm.get('requests', None, mcm_query)
+
+                    if len(requests)>0:
+                        for request in requests:
+                            if opt.verbose:
+                                print 'Request', request['prepid'], 'in status', request['status']
+                            if request['status']=='new': mcm_status = 1
+                            elif request['status']=='validation': mcm_status = 2
+                            elif request['status']=='defined': mcm_status = 3
+		            elif request['status']=='approved': mcm_status = 4
+                            elif request['status']=='submitted': mcm_status = 5                         
+   
+                    if mcm_status<1:
+
+                        print 'Warning: no request found for sample', process+period, 'in McM campaign', campaign['GEN'], 'check PC planning'
+
             sampleName = process if Sim=='' else sample.replace('_newpmx','').split('_ext')[0]
-            sampleName += datasetFlag
+            sampleName += datasetFlag # -> to be refined
 
             OutputSamples[sampleName] = { }
             OutputSamples[sampleName][opt.tier] = datasetFound
