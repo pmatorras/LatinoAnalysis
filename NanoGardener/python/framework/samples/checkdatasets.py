aaabfa93238abc61d6d237c653e371cff79343e0
scodella <luca.scodellaro@cern.ch>
2021-01-29 12:27:17 +0100

    more on check datasets

--- NanoGardener/python/framework/samples/checkdatasets.py
+++ NanoGardener/python/framework/samples/checkdatasets.py
@@ -12,8 +12,12 @@ mcm = McM(dev=True)
 
 if 'pmatorra' in os.environ.get('USER'):
     cmssw_directory= '/afs/cern.ch/work/p/pmatorra/private/CMSSW_10_6_19/'
+    errorcolor = '96m'
+    warningcolor = '92m'
 else:
     cmssw_directory = '/afs/cern.ch/work/s/scodella/SUSY/CMSSW_10_6_19_patch2/'
+    errorcolor = '96m'
+    warningcolor = '92m'
 
 gardening_directory = 'src/LatinoAnalysis/NanoGardener/python/framework/samples/'
 production_directory = 'src/LatinoAnalysis/NanoProducer/python/samples/'
@@ -45,7 +49,7 @@ def substringinlist(sample_list,substring):
     return inlist
 
 if __name__ == '__main__':
-    incsv=[]
+
     csvsamples = readSampleFile("Summer20ULPlanning.csv")
     # Input parameters
     usage = 'usage: %prog [options]'
@@ -56,8 +60,10 @@ if __name__ == '__main__':
     parser.add_option('-c', '--campaign'  , dest='campaign',   help='Campaign'       , default='UL16')
     parser.add_option('-t', '--tier'      , dest='tier',       help='Tier'           , default='nanoAOD')
     parser.add_option('-o', '--outputfile', dest='outputfile', help='Output file'    , default='test')
-    parser.add_option('-v', '--verbose'   , dest='verbose'   , help='Verbose'        , default=False, action='store_true')
+    parser.add_option('-m', '--mute'      , dest='mute'      , help='mute'           , default=False, action='store_true')
     (opt, args) = parser.parse_args()
+
+    csvsamples = readSampleFile("Summer20ULPlanning.csv")
 
     if opt.samplefile==opt.outputfile:
         print 'Error: overwriting input file', opt.samplefile
@@ -66,6 +72,8 @@ if __name__ == '__main__':
         opt.outputfile = opt.outputfile.replace('.py', '')
     run2Samples=False
     isData=False
+
+    verbose = not opt.mute
 
     if 'run2' in opt.campaign:
         campaign_years=campaigns.keys()
@@ -118,10 +126,18 @@ if __name__ == '__main__':
         thistier=opt.tier.upper()+Sim
         print "CAMPAIGN:", campaign[thistier].upper(), thistier
         for sample in Samples:
+
             process = Samples[sample][opt.tier].split('/')[1]
             period = '' if Sim=='SIM' else Samples[sample][opt.tier].split('/')[2].split('-')[0].split('_')[0]
 
-            if opt.verbose: print period
+            if not isData:
+                process = process.replace('_PSweights', '')
+                if 'Tune' not in process: process = process.replace('13TeV', 'TuneCP5_13TeV')
+                process = process.replace('TuneCUETP8M1', 'TuneCP5')
+                process.replace('TuneCUETP8M2', 'TuneCP5')         
+                process = process.replace('_ttHtranche3', '')
+ 
+            if verbose: print '\n', process, period
 
             datasetsFound = [ ] 
             parentsFound = [ ]
@@ -149,11 +165,12 @@ if __name__ == '__main__':
 
             if len(datasetsFound)==1:
                 datasetFound = datasetsFound[0]
-                if opt.verbose:
+                if verbose:
                     print 'Dataset found for sample', process+period, 'in campaign', campaign[thistier], '-->', datasetFound
 
             elif len(datasetsFound)>1:
-                print 'Warning: multiple datasets found for sample', process+period, 'in campaign', campaign[thistier], '-->', datasetsFound
+                if verbose: 
+                    print 'Warning: multiple datasets found for sample', process+period, 'in campaign', campaign[thistier], '-->', datasetsFound
                 version = 0
                 saveset = ''
                 for dataset in datasetsFound:  
@@ -172,27 +189,22 @@ if __name__ == '__main__':
                 #    query_output = subprocess.check_output('dasgoclient -query='+query, shell=True)
                 #exit()
             else:   
-                print 'Warning: no dataset found for sample', process+period, 'in campaign', campaign[thistier]
+                if verbose: 
+                    print 'Warning: no dataset found for sample', process+period, 'in tier', opt.tier, 'for campaign', campaign[thistier] 
                 if len(parentsFound)>0:
-                    print '         available parents are', parentsFound
+                    if verbose:
+                        print '         available parents are', parentsFound
 
-                else:
-                    if isData: continue
-                    incsv=substringinlist(csvsamples,sample)
-                    if   (len(incsv)==0) : continue
-                    elif (len(incsv)>5)  : print "MULTIPLE OPTIONS AVAILABLE FOR THE CSV FILE",
-                    print "SAMPLES IN CSV:",sample, incsv[:5]
+                elif not isData:
                     
                     mcm_status = 0
 
                     mcm_query = 'dataset_name='+process+'&prepid=*'+campaign['GEN']+'*'
-                    mcm_query = mcm_query.replace('_PSweights', '')
-                    if 'Tune' not in mcm_query: mcm_query = mcm_query.replace('13TeV', 'TuneCP5_13TeV')
                     requests = mcm.get('requests', None, mcm_query)
 
                     if len(requests)>0:
                         for request in requests:
-                            if opt.verbose:
+                            if verbose:
                                 print 'Request', request['prepid'], 'in status', request['status']
                             if request['status']=='new': mcm_status = 1
                             elif request['status']=='validation': mcm_status = 2
@@ -202,8 +214,21 @@ if __name__ == '__main__':
    
                     if mcm_status<1:
 
-                        print 'Warning: no request found for sample', process+period, 'in McM campaign', campaign['GEN'], 'check PC planning'
+                        if verbose:
+                            print 'Warning: no request found for sample', process, 'in McM campaign', campaign['GEN'], 'check PC planning'
 
+                        incsv=substringinlist(csvsamples,process)
+                        if   (len(incsv)==0) :
+                            incsvsample = substringinlist(csvsamples,sample)
+                            if len(incsvsample)==0: 
+                                print '\033['+errorcolor + 'Warning: sample', process, 'not in the planned production campaign' + '\033[0m'
+                            else:
+                                print '\033['+warningcolor + 'Warning: sample', process, 'not in the planned production campaign' + '\033[0m'
+                                print '         but alternative samples are there:', incsvsample
+                        elif (len(incsv)>5)  : 
+                            if verbose: print "MULTIPLE OPTIONS AVAILABLE FOR THE CSV FILE"
+                        elif verbose:  print "SAMPLES IN CSV:",sample, incsv[:5]
+ 
             sampleName = process if Sim=='' else sample.replace('_newpmx','').split('_ext')[0]
             sampleName += datasetFlag # -> to be refined
 
