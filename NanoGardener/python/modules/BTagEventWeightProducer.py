515d5aaffd1e958ee425f3e9cf1b32273c0eb062
scodella <luca.scodellaro@cern.ch>
2021-02-04 09:36:10 +0100

    BTagEventWeightProducer: add UL wps and change to new sf names from btagSFProducer

--- NanoGardener/python/modules/BTagEventWeightProducer.py
+++ NanoGardener/python/modules/BTagEventWeightProducer.py
@@ -8,48 +8,37 @@ import math
 from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection 
 from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
 
+bTagWorkingPointCut = { 'btagDeepB'     : { '2016' : { 'L' : '0.2217', 'M' : '0.6321', 'T' : '0.8953' },
+                                            '2017' : { 'L' : '0.1522', 'M' : '0.4941', 'T' : '0.8001' },
+                                            '2018' : { 'L' : '0.1241', 'M' : '0.4184', 'T' : '0.7527' },
+                                            'UL16' : { 'L' : '', 'M' : '', 'T' : '' }, # TO BE UPDATED
+                                            'UL17' : { 'L' : '0.1355', 'M' : '0.4506', 'T' : '0.7738' }, 
+                                            'UL18' : { 'L' : '0.1208', 'M' : '0.4168', 'T' : '0.7665' }, },
+                        'btagDeepFlavB' : { '2016' : { 'L' : '0.0614', 'M' : '0.3093', 'T' : '0.7221' },
+                                            '2017' : { 'L' : '0.0521', 'M' : '0.3033', 'T' : '0.7489' },
+                                            '2018' : { 'L' : '0.0494', 'M' : '0.2770', 'T' : '0.7264' },
+                                            'UL16' : { 'L' : '', 'M' : '', 'T' : '' }, # TO BE UPDATED 
+                                            'UL17' : { 'L' : '0.0532', 'M' : '0.3040', 'T' : '0.7476' },
+                                            'UL18' : { 'L' : '0.0490', 'M' : '0.2783', 'T' : '0.7100' }, }, }
+
 class BTagEventWeightProducer(Module):
-    def __init__(self, collection="Lepton", bTagAlgo="", bTagWP="", dataType='mc', bTagMethod = '1d', bTagPtCut = '20', bTagEff_path=''):
+    def __init__(self, collection="Lepton", bTagEra = "", bTagAlgo="", bTagWP="", dataType='mc', bTagMethod = '1d', bTagPtCut = '20', bTagEff_path=''):
         self.collection = collection
+        self.bTagEra = bTagEra
         self.bTagAlgo = bTagAlgo
         self.random = ROOT.TRandom3(0)
         self.dataType = dataType
         self.bTagMethod = bTagMethod
         self.bTagPtCut = bTagPtCut
         self.bTagEff_path = bTagEff_path
-        self.bTagEtaMax = 2.4 if ('2016' in bTagWP) else 2.5
+        self.bTagEtaMax = 2.4 if (bTagEra=='2016' or bTagEra=='UL16') else 2.5
         self.bTagFlag = ''
         if bTagMethod!='1d':
-            self.bTagFlag = '_' + bTagAlgo + bTagWP[-1] + '_' + bTagMethod
+            self.bTagFlag = '_' + bTagAlgo + '_' + bTagWP + '_' + bTagMethod
             if bTagPtCut!='20': self.bTagFlag += '_Pt' + bTagPtCut
-        print self.bTagFlag
         self.bTagWP = bTagWP
-        if bTagAlgo=='btagDeepB':
-            if '2016' in bTagWP: 
-                if bTagWP[-1]=='L': self.bTagCut = 0.2217
-                if bTagWP[-1]=='M': self.bTagCut = 0.6321
-                if bTagWP[-1]=='T': self.bTagCut = 0.8953
-            elif '2017' in bTagWP: 
-                if bTagWP[-1]=='L': self.bTagCut = 0.1522
-                if bTagWP[-1]=='M': self.bTagCut = 0.4941
-                if bTagWP[-1]=='T': self.bTagCut = 0.8001
-            elif '2018' in bTagWP: 
-                if bTagWP[-1]=='L': self.bTagCut = 0.1241
-                if bTagWP[-1]=='M': self.bTagCut = 0.4184
-                if bTagWP[-1]=='T': self.bTagCut = 0.7527
-        elif bTagAlgo=="btagDeepFlavB": 
-            if '2016' in bTagWP: 
-                if bTagWP[-1]=='L': self.bTagCut = 0.0614
-                if bTagWP[-1]=='M': self.bTagCut = 0.3093
-                if bTagWP[-1]=='T': self.bTagCut = 0.7221
-            elif '2017' in bTagWP: 
-                if bTagWP[-1]=='L': self.bTagCut = 0.0521
-                if bTagWP[-1]=='M': self.bTagCut = 0.3033
-                if bTagWP[-1]=='T': self.bTagCut = 0.7489
-            elif '2018' in bTagWP: 
-                if bTagWP[-1]=='L': self.bTagCut = 0.0494
-                if bTagWP[-1]=='M': self.bTagCut = 0.2770
-                if bTagWP[-1]=='T': self.bTagCut = 0.7264
+        self.bTagCut = 'NA' if bTagMethod=='1d' else bTagWorkingPointCut[bTagAlgo][bTagEra][bTagWP]
+
     def beginJob(self):
         pass
     def endJob(self):
@@ -65,15 +54,18 @@ class BTagEventWeightProducer(Module):
 
         btagVar = 'btagWeight'+self.bTagFlag if (self.bTagMethod[0]=='1') else 'leadingPtTagged'+self.bTagFlag
 
-        if self.bTagMethod=='1c':
-            btagVar = btagVar.replace("Weight", "Weight_1tag")
-            self.out.branch(btagVar,'F') 
+        if self.bTagMethod=='1b' or self.bTagMethod=='1c':
+            btagVar = btagVar.replace("Weight", "Weight_1tag") 
+            self.out.branch(btagVar,'F')
             self.out.branch(btagVar.replace("_1tag", "_2tag"),'F')
 
         self.central_and_systs_shape_corr = [ "central" ]
 
         if self.dataType=='data':
             return True
+
+        if self.bTagMethod=='1a' or self.bTagMethod=='1d':
+            self.out.branch(btagVar,'F')
 
         self.systs_shape_corr = []
         if self.bTagMethod=='1d' :
@@ -103,24 +95,22 @@ class BTagEventWeightProducer(Module):
                 self.branchNames_central_and_systs_shape_corr[central_or_syst] = btagVar
             else:
                 self.branchNames_central_and_systs_shape_corr[central_or_syst] = "%s_%s" % (btagVar, central_or_syst)
-            if central_or_syst!="central" or (self.bTagMethod!='1c' and self.bTagMethod!='2a'):
                 self.out.branch(self.branchNames_central_and_systs_shape_corr[central_or_syst],'F') 
                 
-        if self.dataType!='data':
-            if self.bTagMethod=='1a' or self.bTagMethod=='2a':
-                self.bTagEfficiencies = {}
-                cmssw_base = os.getenv('CMSSW_BASE')
-                btageff_file = self.open_root(cmssw_base + '/src/' + self.bTagEff_path)
-                sample_flag = 'ttbar' if (self.dataType=='mc') else 'T2tt'
-                # These two is because histograms from mkShapes are unrolled
-                self.bTagEfficiencies['jetptbins']  = self.get_root_obj(btageff_file, 'taggablejets_b/jetpt/histo_'+sample_flag)
-                self.bTagEfficiencies['jetetabins'] = self.get_root_obj(btageff_file, 'taggablejets_b/jeteta/histo_'+sample_flag)
-                self.bTagEfficiencies['taggable_b'] = self.get_root_obj(btageff_file, 'taggablejets_b/jetpteta/histo_'+sample_flag)
-                self.bTagEfficiencies['taggable_c'] = self.get_root_obj(btageff_file, 'taggablejets_c/jetpteta/histo_'+sample_flag)
-                self.bTagEfficiencies['taggable_l'] = self.get_root_obj(btageff_file, 'taggablejets_l/jetpteta/histo_'+sample_flag)
-                self.bTagEfficiencies['tagged_b'] = self.get_root_obj(btageff_file, self.bTagAlgo+'_'+self.bTagWP+'_b/jetpteta/histo_'+sample_flag)
-                self.bTagEfficiencies['tagged_c'] = self.get_root_obj(btageff_file, self.bTagAlgo+'_'+self.bTagWP+'_c/jetpteta/histo_'+sample_flag)
-                self.bTagEfficiencies['tagged_l'] = self.get_root_obj(btageff_file, self.bTagAlgo+'_'+self.bTagWP+'_l/jetpteta/histo_'+sample_flag)
+        if self.bTagMethod=='1a' or self.bTagMethod=='1b' or self.bTagMethod=='2a':
+            self.bTagEfficiencies = {}
+            cmssw_base = os.getenv('CMSSW_BASE')
+            btageff_file = self.open_root(cmssw_base + '/src/' + self.bTagEff_path)
+            sample_flag = 'ttbar' if (self.dataType=='mc') else 'T2tt'
+            # These two is because histograms from mkShapes are unrolled
+            self.bTagEfficiencies['jetptbins']  = self.get_root_obj(btageff_file, 'taggablejets_b/jetpt/histo_'+sample_flag)
+            self.bTagEfficiencies['jetetabins'] = self.get_root_obj(btageff_file, 'taggablejets_b/jeteta/histo_'+sample_flag)
+            self.bTagEfficiencies['taggable_b'] = self.get_root_obj(btageff_file, 'taggablejets_b/jetpteta/histo_'+sample_flag)
+            self.bTagEfficiencies['taggable_c'] = self.get_root_obj(btageff_file, 'taggablejets_c/jetpteta/histo_'+sample_flag)
+            self.bTagEfficiencies['taggable_l'] = self.get_root_obj(btageff_file, 'taggablejets_l/jetpteta/histo_'+sample_flag)
+            self.bTagEfficiencies['tagged_b'] = self.get_root_obj(btageff_file, self.bTagAlgo+'_'+self.bTagWP+'_b/jetpteta/histo_'+sample_flag)
+            self.bTagEfficiencies['tagged_c'] = self.get_root_obj(btageff_file, self.bTagAlgo+'_'+self.bTagWP+'_c/jetpteta/histo_'+sample_flag)
+            self.bTagEfficiencies['tagged_l'] = self.get_root_obj(btageff_file, self.bTagAlgo+'_'+self.bTagWP+'_l/jetpteta/histo_'+sample_flag)
 
     def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
         pass
@@ -177,14 +167,16 @@ class BTagEventWeightProducer(Module):
 
         return njets_tagged/njets_taggable
 
-    def getbTagSF(self, event, idx, jfl, central_or_syst):
+    def getbTagSF(self, event, idx, bTagAlgoWP, central_or_syst):
+
+        jfl = event.Jet_hadronFlavour[idx]
 
         central_or_syst_flag = ""
         if (central_or_syst=="b_up" or central_or_syst=="b_down") and (abs(jfl)==5 or abs(jfl)==4):
             central_or_syst_flag = central_or_syst.replace("b", "")
         if (central_or_syst=="l_up" or central_or_syst=="l_down") and abs(jfl)<4:
             central_or_syst_flag = central_or_syst.replace("l", "")
-        jet_weight = getattr(event, "Jet_btagSF%s" % central_or_syst_flag)[idx]
+        jet_weight = getattr(event, "Jet_btagSF_%s%s" % (bTagAlgoWP, central_or_syst_flag))[idx]
         if self.dataType=="fastsim":
             central_or_syst_fastsim_flag = ""
             if "fastsim" in central_or_syst:
@@ -195,7 +187,7 @@ class BTagEventWeightProducer(Module):
                     central_or_syst_fastsim_flag = central_or_syst_temp.replace("c", "")
                 if "l" in central_or_syst_temp and abs(jfl)<4:
                     central_or_syst_fastsim_flag = central_or_syst_temp.replace("l", "") 
-            jet_weight *= getattr(event, "Jet_btagFastSimSF%s" % central_or_syst_fastsim_flag)[idx]
+            jet_weight *= getattr(event, "Jet_btagFastSimSF_%s%s" % (bTagAlgoWP, central_or_syst_fastsim_flag))[idx]
 
         return jet_weight
 
@@ -234,7 +226,7 @@ class BTagEventWeightProducer(Module):
                     
                         if self.dataType!='data' and self.bTagMethod=='2a':
 
-                            jet_weight = self.getbTagSF(event, idx, jfl, central_or_syst)
+                            jet_weight = self.getbTagSF(event, idx, self.bTagAlgo+'_'+self.bTagWP, central_or_syst)
 
                             if jet_weight<1. and bTagPass==True:
                                 if bTagDice<(1.-jet_weight):
@@ -260,7 +252,7 @@ class BTagEventWeightProducer(Module):
         self.out.fillBranch('trailingPtTagged'+self.bTagFlag, trailingPtTaggedValue['central'])
 
         if self.dataType=='data':
-            if self.bTagMethod=='1c':
+            if self.bTagMethod=='1b' or self.bTagMethod=='1c':
                 self.out.fillBranch('btagWeight_1tag'+self.bTagFlag, (leadingPtTaggedValue['central']>=float(self.bTagPtCut)))
                 self.out.fillBranch('btagWeight_2tag'+self.bTagFlag, (trailingPtTaggedValue['central']>=float(self.bTagPtCut)))
             return True
@@ -277,11 +269,11 @@ class BTagEventWeightProducer(Module):
                         #print event.nCleanJet , event.nJet , i , event.CleanJet_jetIdx[i]
                         #weight = weight*event.Jet_btagSF_shape[event.CleanJet_jetIdx[i]]
                         idx = event.CleanJet_jetIdx[i]
-                        weight *= event.Jet_btagSF_shape[idx]
+                        weight *= getattr(event, "Jet_btagSF_%s_shape" % self.bTagAlgo)[idx]
                 else:
                     weight=1.
                     for i in range(event.nCleanJet):
-                        weight = weight*getattr(event, "Jet_btagSF_shape_%s" % central_or_syst)[event.CleanJet_jetIdx[i]]
+                        weight = weight*getattr(event, "Jet_btagSF_%s_shape_%s" % (self.bTagAlgo, central_or_syst))[event.CleanJet_jetIdx[i]]
             else :
                 if central_or_syst=='central':
                     weight1idx = [ ]
@@ -292,13 +284,13 @@ class BTagEventWeightProducer(Module):
                             if getattr(event, "Jet_%s" % self.bTagAlgo)[event.CleanJet_jetIdx[j]]>=self.bTagCut:
                                 weight1idx.append(j)
                                 weight1jet.append(1.)
-                                weightjjet.append(self.getbTagSF(event, event.CleanJet_jetIdx[j], event.Jet_hadronFlavour[j], central_or_syst))
+                                weightjjet.append(self.getbTagSF(event, event.CleanJet_jetIdx[j], self.bTagAlgo+'_'+self.bTagWP, central_or_syst))
                 for i in range(event.nCleanJet):
                     if event.CleanJet_pt[i]>=float(self.bTagPtCut) and abs(event.CleanJet_eta[i])<self.bTagEtaMax:
                         idx = event.CleanJet_jetIdx[i]
                         jfl = event.Jet_hadronFlavour[idx]
                         jet_discriminant = getattr(event, "Jet_%s" % self.bTagAlgo)[idx]
-                        jet_weight = self.getbTagSF(event, idx, jfl, central_or_syst)
+                        jet_weight = self.getbTagSF(event, idx, self.bTagAlgo+'_'+self.bTagWP, central_or_syst)
                         if self.bTagMethod=='1a':
                             if jet_discriminant>=self.bTagCut:
                                 weight *= jet_weight
@@ -307,6 +299,8 @@ class BTagEventWeightProducer(Module):
                                                                   event.CleanJet_eta[i], 
                                                                   jfl)
                                 weight *= (1. - jet_weight*jet_bTagEff)/(1. - jet_bTagEff)
+                        elif self.bTagMethod=='1b': # To be completed
+                            print 'BTagEventWeightProducer: warning, method 1b not yet implemented'
                         elif self.bTagMethod=='1c':
                             if jet_discriminant>=self.bTagCut:
                                 weight *= (1. - jet_weight)
@@ -316,7 +310,7 @@ class BTagEventWeightProducer(Module):
                                             weight1jet[j] *= jet_weight
                                         else:
                                             weight1jet[j] *= (1. - weightjjet[j])
-                if self.bTagMethod=='1c':
+                if self.bTagMethod=='1b' or self.bTagMethod=='1c':
                     weight = 1. - weight
                     if central_or_syst=='central':   
                         weight2tag = weight
@@ -325,6 +319,5 @@ class BTagEventWeightProducer(Module):
                         self.out.fillBranch(self.branchNames_central_and_systs_shape_corr[central_or_syst].replace("Weight_1tag", "Weight_2tag"), weight2tag)  
             self.out.fillBranch(self.branchNames_central_and_systs_shape_corr[central_or_syst], weight)   
             
-
         return True
 
